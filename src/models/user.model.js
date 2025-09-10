54de36eba24b73b9f2c3890e66fdcdb00482b8fd
dhruvil <dhruvil.ctasis.llp@gmail.com>
2025-09-10 15:38:44 +0530

    change user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,8 @@
 import mongoose, {Schema} from "mongoose";
 import { use } from "react";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { emit } from "npm";
 
 
 
@@ -54,8 +57,44 @@ const userSchema = new Schema(
 );
 
 
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+        
+    
+    this.password = bcrypt.hash(this.password , 10)
+    next()
 
+})
 
+userSchema.methods.ispasswordCorrect = async function (password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken= function(){
+    return jwt.sign( 
+        {
+            _id: this._id,
+            emit: this.email,
+            username : this.username,
+            fullname:this.fullname
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+userSchema.methods.generateRefreshToken= function(){
+    return jwt.sign( 
+        {
+            _id: this._id,
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
 
 
 
